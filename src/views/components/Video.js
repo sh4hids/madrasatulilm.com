d818c38b326d0e9ff6b8612068126ecc03181de4
Shahidul Islam Majumder <dev@shahid.pro>
2021-01-21 18:21:00 +0600

    Fix video responsive issue

--- src/views/components/Video.js
+++ src/views/components/Video.js
@@ -4,23 +4,29 @@ import { space, layout, flexbox, color } from 'styled-system';
 
 const VideoContainer = styled.div`
   text-align: center;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 56.25%;
   ${space};
   ${layout};
   ${flexbox};
   ${color};
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
 `;
 
-const Video = ({
-  videoSrcURL,
-  videoTitle,
-  frameHeight,
-  frameWidth,
-  ...props
-}) => (
+const Video = ({ videoSrcURL, videoTitle, ...props }) => (
   <VideoContainer {...props}>
     <iframe
-      height={frameHeight}
-      width={frameWidth}
       src={videoSrcURL}
       title={videoTitle}
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
