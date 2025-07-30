d952824e240d9982349ba1fdd543334e78e21372
Shahidul Islam Majumder <dev@shahid.pro>
2025-07-30 19:17:14 +0600

    fix: homepage title

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://madrasatulilm.com/',
+    site: 'https://milm.netlify.app/',
     base: '/',
     vite: {
         plugins: [tailwindcss()],
